c99024e0225c787ac514c55e81b1a48694ca109b
Alpvax <development@alpvax.uk>
2020-01-29 23:33:21 +0000

    [WIP]Begin adding wire shapes

--- src/main/java/alpvax/advancedautocrafting/block/WireBlock.java
+++ src/main/java/alpvax/advancedautocrafting/block/WireBlock.java
@@ -1,40 +1,89 @@
 package alpvax.advancedautocrafting.block;
 
 import alpvax.advancedautocrafting.craftnetwork.Capabilities;
+import com.google.common.collect.Maps;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.SixWayBlock;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.pathfinding.PathType;
+import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class WireBlock extends SixWayBlock {
+public class WireBlock extends Block {
   private static final Direction[] ALL_DIRECTIONS = Direction.values();
+  public static enum ConnectionState implements IStringSerializable {
+    NONE,
+    CONNECTION,
+    INTERFACE,
+    DISABLED;
+
+    private static final ConnectionState[] VALUES = values();
+    private final String name;
+
+    private ConnectionState() {
+      name = name().toLowerCase(Locale.ENGLISH);
+    }
+
+    @Override
+    public String getName() {
+      return name;
+    }
+  }
+  public static class ConnectionProperty extends EnumProperty<ConnectionState> {
+
+    protected ConnectionProperty(String name) {
+      this(name, Arrays.asList(ConnectionState.VALUES));
+    }
+
+    public <T> ConnectionProperty(String name, Collection<ConnectionState> allowedValues) {
+      super(name, ConnectionState.class, allowedValues);
+    }
+  }
+
+  public static final Map<Direction, EnumProperty> DIR_TO_PROPERTY_MAP = Util.make(Maps.newEnumMap(Direction.class), (map) -> {
+    for(Direction d : ALL_DIRECTIONS) {
+      map.put(d, EnumProperty.create(d.getName(), ConnectionState.class));
+    }
+  });
+
   public static final Set<Block> WIRE_BLOCKS = new HashSet();
 
+  protected VoxelShape coreShape;
+  protected Map<Direction, VoxelShape> wireShapes;
+  protected Map<Direction, VoxelShape> interfaceShapes;
+
   public WireBlock(Properties properties) {
-    super(0.25F, properties);
-    setDefaultState(stateContainer.getBaseState()
-        .with(NORTH, Boolean.valueOf(false))
-        .with(EAST, Boolean.valueOf(false))
-        .with(SOUTH, Boolean.valueOf(false))
-        .with(WEST, Boolean.valueOf(false))
-        .with(UP, Boolean.valueOf(false))
-        .with(DOWN, Boolean.valueOf(false))
-    );
+    super(properties);
+    BlockState state = stateContainer.getBaseState();
+    for(Direction d : ALL_DIRECTIONS) {
+      state = withConnectionState(state, d, ConnectionState.NONE);
+    }
+    setDefaultState(state);
+    setupShapes(0.25, 0.125, 0.375, 0.0625);
     WIRE_BLOCKS.add(this);
+  }
+
+  private BlockState withConnectionState(BlockState bState, Direction dir, ConnectionState cState) {
+    return bState.with(DIR_TO_PROPERTY_MAP.get(dir), cState);
   }
 
   @Override
   protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-    builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN);
+    for(EnumProperty<ConnectionState> prop : DIR_TO_PROPERTY_MAP.values()) {
+      builder.add(prop);
+    }
   }
 
   @Override
@@ -43,8 +92,97 @@ public class WireBlock extends SixWayBlock {
   }
 
   @Override
+  public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+    return false;
+  }
+
+  @Override
   public BlockState getStateForPlacement(BlockItemUseContext context) {
     return this.makeConnections(context.getWorld(), context.getPos());
+  }
+
+  protected void setupShapes(double coreRadius, double wireRadius, double interfaceRadius, double interfaceWidth) {
+    double coreMin = 0.5 - coreRadius;
+    double coreMax = 0.5 + coreRadius;
+    coreShape = VoxelShapes.create(coreMin, coreMin, coreMin, coreMax, coreMax, coreMax);
+    wireShapes = new HashMap<>();
+    interfaceShapes = new HashMap<>();
+
+    for(Direction d : ALL_DIRECTIONS) {
+      Vec3i vec = d.getDirectionVec();
+      int x = vec.getX();
+      int y = vec.getY();
+      int z = vec.getZ();
+      double wminx, wmaxx, wminy, wmaxy, wminz, wmaxz; // Wire
+      double iminx, imaxx, iminy, imaxy, iminz, imaxz; // Interface
+      if(x == 0) {
+        wminx = 0.5 - wireRadius;
+        wmaxx = 0.5 + wireRadius;
+        iminx = 0.5 - interfaceRadius;
+        imaxx = 0.5 + interfaceRadius;
+      } else if(x < 0) {
+        wminx = 0;
+        wmaxx = coreMin;
+        iminx = 0;
+        imaxx = interfaceWidth;
+      } else {
+        wminx = coreMax;
+        wmaxx = 1;
+        iminx = 1 - interfaceWidth;
+        imaxx = 1;
+      }
+      if(y == 0) {
+        wminy = 0.5 - wireRadius;
+        wmaxy = 0.5 + wireRadius;
+        iminy = 0.5 - interfaceRadius;
+        imaxy = 0.5 + interfaceRadius;
+      } else if(y < 0) {
+        wminy = 0;
+        wmaxy = coreMin;
+        iminy = 0;
+        imaxy = interfaceWidth;
+      } else {
+        wminy = coreMax;
+        wmaxy = 1;
+        iminy = 1 - interfaceWidth;
+        imaxy = 1;
+      }
+      if(z == 0) {
+        wminz = 0.5 - wireRadius;
+        wmaxz = 0.5 + wireRadius;
+        iminz = 0.5 - interfaceRadius;
+        imaxz = 0.5 + interfaceRadius;
+      } else if(z < 0) {
+        wminz = 0;
+        wmaxz = coreMin;
+        iminz = 0;
+        imaxz = interfaceWidth;
+      } else {
+        wminz = coreMax;
+        wmaxz = 1;
+        iminz = 1 - interfaceWidth;
+        imaxz = 1;
+      }
+      wireShapes.put(d, VoxelShapes.create(wminx, wminy, wminz, wmaxx, wmaxy, wmaxz));
+      interfaceShapes.put(d, VoxelShapes.create(iminx, iminy, iminz, imaxx, imaxy, imaxz));
+    }
+  }
+
+  @Override
+  public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+    List<VoxelShape> shapes = new ArrayList<>();
+    for(Direction d : ALL_DIRECTIONS) {
+      EnumProperty<ConnectionState> prop = DIR_TO_PROPERTY_MAP.get(d);
+      switch(state.get(prop)) {
+        case INTERFACE:
+          shapes.add(interfaceShapes.get(d));
+          // Do not break, need to add wire too
+        case CONNECTION:
+          shapes.add(wireShapes.get(d));
+          break;
+      }
+    }
+    return VoxelShapes.or(coreShape, shapes.toArray(new VoxelShape[0]));
   }
 
   public BlockState makeConnections(IBlockReader world, BlockPos thisPos) {
@@ -54,7 +192,7 @@ public class WireBlock extends SixWayBlock {
       TileEntity tile = world.getTileEntity(pos);
       Block block = world.getBlockState(pos).getBlock();
       if(WIRE_BLOCKS.contains(block) || (tile != null && tile.getCapability(Capabilities.NODE_CAPABILITY).isPresent())) {
-        state.with(FACING_TO_PROPERTY_MAP.get(d), true);
+        state.with(DIR_TO_PROPERTY_MAP.get(d), true);
       }
     }
     return state;
--- src/main/java/alpvax/advancedautocrafting/client/data/AABlockstateProvider.java
+++ src/main/java/alpvax/advancedautocrafting/client/data/AABlockstateProvider.java
@@ -1,10 +1,14 @@
 package alpvax.advancedautocrafting.client.data;
 
 import alpvax.advancedautocrafting.AdvancedAutocrafting;
+import alpvax.advancedautocrafting.block.WireBlock;
 import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.util.Direction;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
+import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
 
 import java.util.function.Supplier;
 
@@ -21,10 +25,36 @@ public class AABlockstateProvider extends BlockStateProvider {
     simpleBlock(CONTROLLER);
     simpleBlock(REMOTE_MARKER);
     simpleBlock(REMOTE_MASTER);
-    simpleBlock(WIRE); //TODO: make non-simple
+    wireBlock(WIRE);
   }
 
   private void simpleBlock(Supplier<Block> sup) {
     simpleBlock(sup.get());
+  }
+
+  protected void wireBlock(Supplier<Block> sup, ModelFile core, ModelFile wire, ModelFile iface) {
+    MultiPartBlockStateBuilder builder = getMultipartBuilder(sup.get()).part().modelFile(core).addModel().end();
+    WireBlock.DIR_TO_PROPERTY_MAP.entrySet().forEach(e -> {
+      Direction dir = e.getKey();
+      int yrot = (((int) dir.getHorizontalAngle()) + 180) % 360;
+      int xrot = dir.getXOffset() == 0 ? 0 : dir.getXOffset() < 0 ? 270 : 90;
+      builder.part()
+              .modelFile(wire)
+              .rotationY(yrot)
+              .rotationX(xrot)
+              .uvLock(true)
+              .addModel()
+              .condition(e.getValue(),
+                      WireBlock.ConnectionState.CONNECTION,
+                      WireBlock.ConnectionState.INTERFACE
+              );
+      builder.part()
+              .modelFile(iface)
+              .rotationY(yrot)
+              .rotationX(xrot)
+              .uvLock(true)
+              .addModel()
+              .condition(e.getValue(), WireBlock.ConnectionState.INTERFACE);
+    });
   }
 }
