a3e878fd7fa8a351b14bebb20dee48e3a94b0a41
Alpvax <development@alpvax.uk>
2021-10-05 17:14:43 +0100

    Update to forge 37.0.82
    Allows stacked GUIs.
    Updated deprecated @CapabilityInject to CapabilityManager::get

--- src/main/java/alpvax/advancedautocrafting/Capabilities.java
+++ src/main/java/alpvax/advancedautocrafting/Capabilities.java
@@ -3,15 +3,14 @@ package alpvax.advancedautocrafting;
 import alpvax.advancedautocrafting.craftnetwork.INetworkNode;
 import alpvax.advancedautocrafting.item.IMultitool;
 import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityToken;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 
 public class Capabilities {
-  @CapabilityInject(INetworkNode.class)
-  public static Capability<INetworkNode> NODE_CAPABILITY = null;
+  public static Capability<INetworkNode> NODE_CAPABILITY = CapabilityManager.get(new CapabilityToken<>(){});
 
-  @CapabilityInject(IMultitool.class)
-  public static Capability<IMultitool> MULTITOOL_CAPABILITY = null;
+  public static Capability<IMultitool> MULTITOOL_CAPABILITY = CapabilityManager.get(new CapabilityToken<>(){});
 
   public static void register(RegisterCapabilitiesEvent event) {
     event.register(INetworkNode.class);
